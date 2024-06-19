58df30d4ebe818146b5a97d50ea66a8659cde843
AlgerP572 <Alger_P@pacbell.net>
2024-06-19 11:29:37 -0700

    google verify

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "826"
+  #define BUILD_NUMBER "831"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.7.826 - 2024-05-24 19:49:20.414965"
+  #define VERSION "v1.7.831 - 2024-06-04 20:21:55.362841"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.7.826"
+  #define VERSION_SHORT "v1.7.831"
 #endif
